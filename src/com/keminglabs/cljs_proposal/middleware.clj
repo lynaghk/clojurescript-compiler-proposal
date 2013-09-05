1ac43ccc9a22a0f65fcd199a8ca86c590d8f9188
Kevin J. Lynagh and Scott Becker <kevin@keminglabs.com>
2013-09-05 22:41:08 +0000

    Add with-analysis middleware.

--- src/com/keminglabs/cljs_proposal/middleware.clj
+++ src/com/keminglabs/cljs_proposal/middleware.clj
@@ -1,11 +1,49 @@
 (ns com.keminglabs.cljs-proposal.middleware
-  (:require [cljs.analyzer :as ana])
+  (:require [cljs.analyzer :as ana]
+            [clojure.set :refer [subset?]])
   (:import java.io.StringReader))
 
 
 (defn with-forms
-  "Reads forms from :cljs-src key of provided `compilation-map` and adds result to :forms key."
+  "Adds :forms key to the compilation map, corresponding to forms read from :cljs-src string."
   [compilation-map]
   ;;TODO document ex-info map shape
   (let [{:keys [cljs-src] :as m} compilation-map]
     (assoc m :forms (ana/forms-seq (StringReader. cljs-src)))))
+
+
+
+(defn expression-map?
+  "From defacto specification in docstring of cljs.analyzer/analyze."
+  [x]
+  (subset? #{:form :op :env} (set (keys x))))
+
+(def namespace-map-keys
+  #{:name :doc :requires :requires-macros :uses :uses-macros :imports :excludes})
+(defn namespace-map?
+  [x]
+  (subset? namespace-map-keys (set (keys x))))
+
+(def cljs-user-namespace-map
+  {:name 'cljs.user
+   :doc nil
+   :requires nil
+   :requires-macros nil
+   :uses nil
+   :uses-macros nil
+   :imports nil
+   :excludes #{}})
+
+(defn with-analysis
+  "Adds :expressions and :namespace keys to the compilation map, from analysis of :forms.
+   All forms should be part of the same namespace."
+  [compilation-map]
+  (let [{:keys [forms] :as m} compilation-map
+        expressions (binding [ana/*cljs-ns* 'cljs.user]
+                      (doall (for [form forms]
+                               (ana/analyze (ana/empty-env) form))))
+        namespace (-> (or (first (filter namespace-map? expressions))
+                          cljs-user-namespace-map)
+                      (select-keys namespace-map-keys))]
+
+    (assoc m :expressions expressions :namespace namespace)))
--- test/com/keminglabs/cljs_proposal/t_middleware.clj
+++ test/com/keminglabs/cljs_proposal/t_middleware.clj
@@ -8,3 +8,24 @@
 ;;with-forms should add seq of forms to compilation map
 (is (= (with-forms {:cljs-src cljs-sample})
        {:cljs-src cljs-sample :forms '((foo 1 2) (bar 3 4))}))
+
+
+;;with-analysis
+
+(let [{:keys [expressions namespace] :as m} (with-analysis {:cljs-src cljs-sample :forms '((foo 1 2) (bar 3 4))})]
+  (is (every? identity #{expressions namespace}))
+  (is (every? expression-map? expressions))
+  (is (namespace-map? namespace)))
+
+;;with a complex namespace
+(is (= (:namespace (with-analysis {:forms '((ns foo
+                                              "Awesome namespace"
+                                              (:require a.b)))}))
+       {:name 'foo
+        :doc "Awesome namespace"
+        :requires '{a.b a.b}
+        :requires-macros nil
+        :uses nil
+        :uses-macros nil
+        :imports nil
+        :excludes #{}}))

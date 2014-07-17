a54b486a955b408cc04ee68818c43f1aaadcfc60
Kevin J. Lynagh <kevin@keminglabs.com>
2014-07-16 22:08:23 -0700

    Use Closure to sort dependencies.
    No need to use its JSModule class directly; we can make SourceFiles (they don't actually need to be files...).

--- src/com/keminglabs/cljs_proposal/condense.clj
+++ src/com/keminglabs/cljs_proposal/condense.clj
@@ -1,20 +1,8 @@
 (ns com.keminglabs.cljs-proposal.condense
   (:require [com.keminglabs.cljs-proposal.middleware :as m]
-            [cljs.closure :as closure])
-  (:import (com.google.javascript.jscomp JSModule SourceFile)))
-
-(defn with-js-module
-  "Adds :js-module key to compilation map with instance of Google's JSModule, corresponding to :js source."
-  [compilation-map]
-  (let [{js :js
-         {ns-symbol :name ns-requires :requires} :namespace} compilation-map
-         ns-name (name ns-symbol)]
-
-    (assert (not (empty? js)) "Compilation map doesn't contain JavaScript code under the :js key.")
-
-    (assoc compilation-map
-      :js-module (doto (JSModule. ns-name)
-                   (.add (SourceFile/fromCode ns-name js))))))
+            [cljs.closure :as closure]
+            [cljs.js-deps :as deps])
+  (:import (com.google.javascript.jscomp DependencyOptions SourceFile)))
 
 (defn compile-cljs
   "Fully realized compilation map for provided ClojureScript string."
@@ -24,41 +12,27 @@
       m/with-analysis
       m/with-js))
 
-(defn add-dependencies-to-js-modules!
-  "Wires up collection of compilation maps containing :js-module keys to depend on each other by bashing on JSModule objects.
-Assumes each module provides a single namespace.
-Returns nil.
-See: http://javadoc.closure-compiler.googlecode.com/git/com/google/javascript/jscomp/JSModule.html
-Throws ex-info if unmet or circular dependencies."
-  [compilation-maps]
-  ;;TODO: throw ex-info with helpful data about unmet or circular dependencies
-  (let [modules-by-ns (into {} (for [m compilation-maps]
-                                 [(get-in m [:namespace :name]) (:js-module m)]))]
-    (doseq [m compilation-maps
-            dep (-> m :namespace :requires vals)]
-      (.addDependency (:js-module m) (modules-by-ns dep)))))
-
 (defn optimize
   "Condenses given compilation maps into a single JavaScript string using the Google Closure compiler."
   [compilation-maps]
   ;;TODO: accept options
   (let [closure-compiler (closure/make-closure-compiler)
 
-        compiler-options (closure/make-options {:optimizations :whitespace})
-
+        compiler-options (doto (closure/make-options {:optimizations :whitespace})
+                           (.setClosurePass true)
+                           (.setDependencyOptions (doto (DependencyOptions.)
+                                                    (.setDependencySorting true))))
         externs []
-        js-modules (-> (map (comp :js-module with-js-module) compilation-maps)
-                       (doto add-dependencies-to-js-modules!)
-                       JSModule/sortJsModules
-                       seq)]
+        js-source-files (for [{:keys [js namespace]} compilation-maps]
+                          (SourceFile/fromCode (str (:name namespace)) js))]
 
-    (let [res (.compileModules closure-compiler externs js-modules compiler-options)]
-      (when (.success res)
+    (let [res (.compile closure-compiler externs js-source-files compiler-options)]
+      (if (.success res)
         (.toSource closure-compiler)
-        ;;TODO: throw errors
-        ))))
 
-
+        ;;TODO: convert errors to ClojureScript data so they can be easily consumed by downstream toolin'
+        (doseq [e (.errors res)]
+          (prn (.-description e)))))))
 
 
 
@@ -67,10 +41,19 @@ Throws ex-info if unmet or circular dependencies."
   (require '[clojure.pprint :refer [pprint]]
            '[clojure.java.io :as io])
 
+  (def compile-cljs*
+    (memoize compile-cljs))
 
-  (->> ["sample/a.cljs" "sample/b.cljs"]
+  (->> [;; (io/resource "cljs/core.cljs")
+        "sample/a.cljs" "sample/b.cljs"]
        (map slurp)
-       (map compile-cljs)
-       optimize)
+       ;;TODO: disable ClojureScript's built in warnings, which are printed during analysis.
+       ;;Linting and warning emission should be separate functions that are invoked with sets of compilation maps.
+       (map compile-cljs*)
+
+       optimize
+       println
+       ;;(spit "foo.js")
+       )
 
   )

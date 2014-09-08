56e7b25e2637e8a829ba11f935753f48b8b36dc5
Kevin J. Lynagh <kevin@keminglabs.com>
2014-09-08 15:31:27 -0700

    Update to ClojureScript 2322.

--- project.clj
+++ project.clj
@@ -2,5 +2,5 @@
   :description "Structure the ClojureScript compiler in terms of namespace-level middleware"
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-2268"
+                 [org.clojure/clojurescript "0.0-2322"
                   :exclusions [org.apache.ant/ant]]])
